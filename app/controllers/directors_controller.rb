b900f8aacc85c32955f67b1c705bfbd319685165
latifvaliyev <102619604+latifvaliyev@users.noreply.github.com>
2024-11-09 19:03:32 +0000

    adding director form etc progress

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,75 @@
 class DirectorsController < ApplicationController
+
+  def update
+    # Get the ID out of params
+    m_id = params.fetch("the_director_id")
+
+    # Look up the existing record id,image,title,year,director
+    matching_records = Director.where({ :id => m_id })
+    the_director = matching_records.at(0)
+
+    # Overwrite each column with the values from user inputs
+    the_director.title = params.fetch("the_title")
+    the_director.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    # Save
+
+    the_movie.save
+
+    # Redirect to the movie details page
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
+
+  def create
+    # params hash looks like this:
+    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+
+    # Retrieve the user's inputs from params
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+
+    # Redirect the user back to the /movies URL
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
+
+
+
+
+
+
+
+
+
+
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
